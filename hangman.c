fe10ec339d52eb6d801477030e8a9fbc9db015e9
YounggyuKim <53363554+YounggyuKim@users.noreply.github.com>
2019-12-05 15:42:10 +0900

    Update hangman.c

--- hangman.c
+++ hangman.c
@@ -3,29 +3,61 @@
 
 int GetWord(char* targetEng, char* targetKor)
 {
+	// 랜덤넘버
 	int RandomNumber;
+	// 파일포인터
 	FILE* fp;
-	char day[sizeof(int) + 1];
+	// 파일명들
+	char FileNames[10] = { 0 };
+	// 파일안에 들어있는 단어 개수
+	int wordCount = 0;
+	// 기타변수
 	int i;
 	char tempStr[ENG_LEN + KOR_LEN];
-
+	char Day[2] = { 0 };
+	
 
 	// 시드 설정
 	srand((unsigned int)time(NULL));
-	// 1 ~ 30 까지중 랜덤한 숫자 하나 선택1
-	RandomNumber = rand() % 30 + 1;
 
-	// 파일명 입력
+	// 파일 입력
 	printf("파일명(일차) : ");
-	scanf("%s", day);
-
-	if (!(fp = fopen(strcat(day, ".dic"), "r")))
+	scanf("%s", Day);
+	
+	// 파일명 만들기
+	strcpy(FileNames, Day);
+	strcat(FileNames, ".dic");
+	
+	// 파일 오픈
+	fp = fopen(FileNames, "r");
+	if (fp == NULL)
 	{
+		// 오픈 에러
 		printf(" >> 파일 읽기 에러 : 해당 파일이 없습니다. <<\n");
-		sleep(2);
 		return FILE_ERROR;
 	}
 
+	// 단어의 개수를 센다.
+	while(!feof(fp))
+	{
+		fgets(tempStr, ENG_LEN + KOR_LEN, fp);
+		wordCount++;
+	}
+	
+	if(wordCount <= 1)
+	{
+		if(!((tempStr[0] >=65 && tempStr[0] <= 90) || (tempStr[0] >= 97 && tempStr[0] <= 122)))
+		{
+			return NO_WORD;
+		}
+	}
+
+	// 파일포인터를 다시 처음으로
+	fseek(fp, 0, SEEK_SET);
+
+	// 1 ~ 30 까지중 랜덤한 숫자 하나 선택1
+	RandomNumber = rand() % wordCount + 1;
+		
 	// 랜덤한 숫자까지 읽어서 tempStr 에 저장
 	for (i = 0; i < RandomNumber; i++)
 	{
@@ -46,13 +78,13 @@ int GetWord(char* targetEng, char* targetKor)
 
 
 // 현재 상태 출력
-void PrintHangman(int ErrorCount)
+void PrintState(int ErrorCount)
 {
 	if (ErrorCount >= 0) printf("-------------------+\n");
 	if (ErrorCount >= 1) printf("                   O\n");
 	if (ErrorCount >= 2) printf("                 / ");
 	if (ErrorCount >= 3) printf("|");
-	if (ErrorCount >= 4) printf("\\\n");
+	if (ErrorCount >= 4) printf(" \\\n");
 	if (ErrorCount >= 5) printf("                  /");
 	if (ErrorCount >= 6) printf(" \\");
 
@@ -99,7 +131,7 @@ void PrintAnswer(char* targetEng, char* Answer, int* ErrorCount)
 		}
 	}
 
-	// 틀렸으면 ErrorCount 증가
+	// 실수했으면 ErrorCount 증가
 	if (IsFind == 0)
 	{
 		(*ErrorCount)++;
@@ -113,54 +145,64 @@ void Hangman()
 	char targetEng[15];
 	// 뜻, 한글 저장
 	char targetKor[60]; 
-	// 틀린 횟수
+	// 에러 횟수
 	int ErrorCount = 0;
 	// 정답
 	char Answer[15] = { 0 };
 	// 기타 변수 
 	int i;
+	int Error;
 	
 	// 파일에서 랜덤한 단어 얻어오기
-	GetWord(targetEng, targetKor);
-
-	// 정답 초기셋팅
-	for (i = 0; i < strlen(targetEng); i++)
+	Error = GetWord(targetEng, targetKor);
+	if(Error == NO_WORD)
 	{
-		Answer[i] = '_';
+		printf(" >> 데이터 파일 에러 : 입력한 파일에 아무 단어도 없습니다. <<\n\n");
 	}
-
-	// 6번 실패할때까지
-	while (ErrorCount != 7)
+	else if(Error == FILE_ERROR)
 	{
-		// 화면 초기화
-		system("clear");
-		// 제목 출력
-		printf(">> 영어 단어 암기 프로그램 : 헹맨 << \n");
-		// 힌트 출력
-		printf("(힌트) %s\n", targetKor);
-		// 현재 상태 출력
-		PrintHangman(ErrorCount);
-
-		// 도전횟수 모두 사용
-		if (ErrorCount == 6)
-			break;
-
-		// 정답 출력 및 게임하기
-		PrintAnswer(targetEng, Answer, &ErrorCount);
-
-		// 정답을 맞추었다면 종료
-		if (strcmp(targetEng, Answer) == 0)
+	}
+	else
+	{
+		// 정답 초기셋팅
+		for (i = 0; i < strlen(targetEng); i++)
 		{
-			printf("   ###########################\n");
-			printf("   #### Congratulations!! ####\n");
-			printf("   ###########################\n");
-			break;
+			Answer[i] = '_';
+		}
+
+		// 6번 실패할때까지
+		while (ErrorCount != 7)
+		{
+			// 화면 초기화
+			system("clear");
+			// 제목 출력
+			printf(">> 영어 단어 암기 프로그램 : 헹맨 << \n");
+			// 힌트 출력
+			printf("(힌트) %s\n", targetKor);
+			// 현재 상태 출력
+			PrintState(ErrorCount);
+
+			// 도전횟수 모두 사용
+			if (ErrorCount == 6)
+				break;
+
+			// 정답 출력 및 게임하기
+			PrintAnswer(targetEng, Answer, &ErrorCount);
+
+			// 정답을 맞추엇다면 종료
+			if (strcmp(targetEng, Answer) == 0)
+			{
+				printf("   ###########################\n");
+				printf("   #### Congratulations!! ####\n");
+				printf("   ###########################\n");
+				break;
+			}
 		}
 	}
-
+	
 	getchar();
 
 	// 마지막 엔터키
-	printf("엔터키를 입력하면 메뉴로 돌아갑니다.\n");
+	printf("\nENTER 키 입력 시 Main 화면으로 돌아갑니다.\n");
 	while (getchar() != '\n');
 }
